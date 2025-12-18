2df56ed62a6eac5d04e090af2c8773d5ac4ac6ab
K.SOMA CHINNA SHEKHAR <shekharkubbireddy@gmail.com>
2025-12-18 17:54:28 +0530

    Time: 3 ms (81.90%) | Memory: 43.3 MB (41.61%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+         Stack<Character> st = new Stack<>();
+            int n = s.length();
+            for(int i=0;i<n;i++){
+                char c = s.charAt(i);
+                if(c == '(' || c == '{' || c == '['){
+                    st.push(c);
+                }else{
+                    if(st.isEmpty()){
+                        return false;
+                    }
+
+                    char ch = st.pop();
+                    if(c == ')' && ch == '(' || c == '}' && ch == '{' || c == ']' && ch =='['){
+                        continue;
+                    }else{
+                        return false;
+                    }
+                }
+            }
+
+            if(st.isEmpty()){
+                return true;
+            }
+        return false;
+
+    }
+}
