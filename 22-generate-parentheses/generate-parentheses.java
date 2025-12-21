9dde279d9dd0ba7f97765c13b06aa94ef0dfa323
K.SOMA CHINNA SHEKHAR <shekharkubbireddy@gmail.com>
2025-12-21 22:16:49 +0530

    Time: 3 ms (15.74%) | Memory: 44.9 MB (17.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,11 +1,11 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
          List<String> list = new ArrayList<>();
-        generateAll(list, "", 0, 0, 1,n);
+        generateAll(list, "", 0, 0,n);
         return list;
     }
 
-    public void generateAll(List<String> li, String s, int open, int close, int idx, int n){
+    public void generateAll(List<String> li, String s, int open, int close, int n){
         if(open > n){
             return;
         }
@@ -14,9 +14,9 @@ class Solution {
             li.add(s);
             return;
         }
-        generateAll(li, s+'(', open+1, close, idx+1, n);
+        generateAll(li, s+'(', open+1, close, n);
         if(open > close){
-            generateAll(li, s+')', open, close+1, idx+1, n);
+            generateAll(li, s+')', open, close+1, n);
         }
     }
 }
