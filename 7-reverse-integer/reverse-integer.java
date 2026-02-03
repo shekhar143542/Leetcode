588a1ee09b11b9271a4821abefbc8ec2439c34fb
K.SOMA CHINNA SHEKHAR <shekharkubbireddy@gmail.com>
2026-02-03 17:44:58 +0530

    Time: 1 ms (99.72%) | Memory: 42.7 MB (39.67%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -6,8 +6,9 @@ class Solution {
         while(x!=0){
 
         rem = x%10;
-        if (sum > Integer.MAX_VALUE / 10 || (sum == Integer.MAX_VALUE / 10 && rem > 7)) return 0;
-        if (sum < Integer.MIN_VALUE / 10 || (sum == Integer.MIN_VALUE / 10 && rem < -8)) return 0;
+       if (Math.abs(sum) > 214748364) {
+    return 0;
+}
 
         sum = sum*10+rem;
         x=x/10;
